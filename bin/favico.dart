d9c258e7ccf0ffd2dd1b0f46e1d5da2652171b7c
Thomas Caserta <tomcaserta@gmail.com>
2014-02-11 12:49:19 +0000

    Added default animations.. still testing

--- bin/favico.dart
+++ bin/favico.dart
@@ -1,8 +1,29 @@
 import 'dart:html';
 import 'dart:async';
+import 'dart:math';
 
 void main () {
-  new Favicon();
+  window.onLoad.listen((ev) { 
+  ImageElement img = new ImageElement();
+  Favicon icon = new Favicon(destinationElement: img, size: 64);
+  FaviconBadge badge = new FaviconBadge ();
+  icon.addElement(new FaviconBackgroundSource (new RGBA (0, 255, 0)));
+  icon.addElement(badge);
+  
+  document.body.append(img);
+    
+    ButtonElement b = new ButtonElement();
+    b.innerHtml = "+ 1";
+    b.onClick.listen((ev) { 
+      badge..slideInUp(200)
+           ..incrementBadge();
+    });
+    document.body.append(b);
+  
+  });
+  
+  
+  
 }
 
 class Favicon {
@@ -10,14 +31,16 @@ class Favicon {
   CanvasRenderingContext2D _context;
   List<FaviconDrawable> elements = new List<FaviconDrawable>();
   Element destinationElement;
+  bool updateFavicon = false;
   int size;
   Favicon ({ this.destinationElement, this.size: 16 }) {
     if (this.destinationElement == null) {
-      // TODO: Set default
+      updateFavicon = true;
     }
     
     _canvas = new CanvasElement();
     _canvas.width = this.size;
+    _canvas.height = this.size;
     _context = _canvas.getContext("2d");    
     FaviconDrawable._init();    
     window.requestAnimationFrame(_beginLoop);
@@ -31,44 +54,91 @@ class Favicon {
     num timeElapsed = t - _prevFrameTime;
     _prevFrameTime = t;
     int eleLength = elements.length;
-    for (int x = 0; x <= eleLength; x++) { 
+    for (int x = 0; x < eleLength; x++) { 
       FaviconDrawable currentDrawable = elements[x];
-      currentDrawable.update(timeElapsed);
+      currentDrawable._update(timeElapsed);
       if (!currentDrawable._remove) {
         currentDrawable.draw(_context);
       }
+      else {
+        elements.removeAt(x);
+        x--;
+        eleLength--;
+      }
+    }
+    updateImage (_canvas.toDataUrl());
+  }
+  
+  void updateImage (String base64Image) {
+    if (!updateFavicon) {
+      destinationElement.setAttribute("src", base64Image);
     }
   }
+  void addElement (FaviconDrawable drawable) {
+    drawable._parent = this;
+    this.elements.add(drawable);
+  }
 }
-typedef bool FaviconTransition (FaviconDrawable drawable, int deltaT, List parameters, FaviconTransitionQueue currentQueue);
-
+typedef bool FaviconTransition (FaviconDrawable drawable, num deltaT, List parameters, FaviconTransitionQueueItem item);
 @proxy
 abstract class FaviconDrawable {
  bool _remove = false;
  List<FaviconTransitionQueue> _animationQueue = new List<FaviconTransitionQueue>();
  FaviconTransitionQueue _currentQueue = new FaviconTransitionQueue();
  bool _isTransitioning = false; 
- num x;
- num y;
- num scale;
- num opacity; 
+ num x = 0;
+ num y = 0;
+ num scale = 1;
+ num opacity = 0; 
  
  num targetX = 0;
  num targetY = 0;
  num targetScale = 1;
  num targetOpacity = 1;
  
- void play () {
+ Favicon _parent;
+ 
+ Favicon get parent => _parent;
+ 
+ num _size;
+ set size (num s) {
+   _size = s;
+ }
+ 
+ num get size {
+   return (_size != null ? _size : parent.size);
+ }
+ 
+ num get scaledSize {
+   return size * scale;
+ }
+ 
+ FaviconDrawable () {
+   
+ }
+ 
+ 
+ Future<FaviconDrawable> play () {
    this._isTransitioning = true;
+   Completer c = _currentQueue.c;
    _animationQueue.add(_currentQueue);
    this.clearCurrent();
+   return c.future;   
+ }
+ 
+ Future<FaviconDrawable> insertAndPlay () {
+   this._isTransitioning = true;
+   Completer c = _currentQueue.c;
+   _animationQueue.insert(0, _currentQueue);
+   this.clearCurrent();
+   return c.future;   
  }
  
  void clearCurrent () {
    _currentQueue = new FaviconTransitionQueue();
  }
  void stop () {
-   _currentState.animationComplete = true;
+   this._isTransitioning = false;
    this.clearCurrent();
    _animationQueue = new List<FaviconTransitionQueue>();
  }
@@ -77,27 +147,50 @@ abstract class FaviconDrawable {
    _isTransitioning = false;
  }
  
+ void resume () {
+   _isTransitioning = true;
+ }
+ 
  void destroy () {
    this._remove = true;
  }
  
- void _update (int timeSinceLastFrame) {
+ void _update (num timeSinceLastFrame) {
    if (_isTransitioning) { 
      if (_animationQueue.length > 0) {
        FaviconTransitionQueue currentAnimationQueue = _animationQueue[0];
-       if (currentAnimationQueue.transitions.length < 0) {
-         FaviconTransitionQueueItem currentAnimation = currentAnimationQueue.transitions[0];
-         bool isComplete = FaviconDrawable._transitions[currentAnimation.animationName](this, currentAnimation.parameters, timeSinceLastFrame, currentAnimationQueue);
-         if (isComplete) currentAnimation.c.complete(this);
+       int animLength = currentAnimationQueue.transitions.length;
+       if (animLength > 0) {
+         bool allComplete = true;
+         for (int animX = 0; animX < animLength; animX++) {
+           FaviconTransitionQueueItem currentAnimation = currentAnimationQueue.transitions[animX];
+           bool isComplete = FaviconDrawable._transitions[currentAnimation.animationName](this, timeSinceLastFrame, currentAnimation.parameters, currentAnimation);
+           currentAnimation.isFirstFrame = false;
+           if (isComplete) {
+             currentAnimation.c.complete(this);
+             currentAnimationQueue.transitions.removeAt(animX);
+             animX--;
+             animLength--;
+           }
+           else allComplete = false;
+         }
+         if (allComplete) {
+           this._isTransitioning = false;
+           this.onAnimationQueueEnd();
+           currentAnimationQueue.c.complete(this);
+           _animationQueue.removeAt(0);
+         }
        }
        else {
          _animationQueue.removeAt(0);
-         this.onAnimationQueueEnd();
          // Reinvoke the update method
          this._update(timeSinceLastFrame);
          // Leave this one so it doesnt update twice...
          return;
        }
+     }
+     else {
+        this.onAnimationQueueEnd();
      }
    }
    
@@ -108,12 +201,14 @@ abstract class FaviconDrawable {
    
  }
  
- void update (int timeSinceLastFrame) {
+ void update (double timeSinceLastFrame) {
   
  }
+ 
  void draw (CanvasRenderingContext2D ctx) {
    
  }
+ 
  
  //***** Transition loading and registration
  static Map<Symbol, FaviconTransition> _transitions = new Map<Symbol, FaviconTransition> ();
@@ -127,13 +222,133 @@ abstract class FaviconDrawable {
  static bool _initialized = false;
  static void _init () {
    if (!_initialized) {
-     // TODO: Register animations
+     bool slide (FaviconDrawable drawable, num deltaT, List parameters, FaviconTransitionQueueItem item) {
+              //Map<String, bool>
+              int duration = parameters.length > 1 && parameters[1] is int ? parameters[1] : 1000;
+              bool isIn = parameters.length > 2 && parameters[2] is bool ? parameters[2] : true;
+              Map<String, bool> directions = parameters[0];
+              bool isComplete = true;
+              
+              if (directions.containsKey("left") || directions.containsKey("right")) {
+               bool isLeft = directions["left"] == true;
+               
+               // Reset our target location so we can slide in or out
+               if (item.isFirstFrame) {
+                   if (isIn) {
+                     drawable.targetX = 0;
+                     drawable.x = (isLeft  == true ? drawable.scaledSize : 0 - drawable.scaledSize);
+                    
+                   }
+                   else drawable.x = (isLeft  == true ? 0 - drawable.scaledSize : drawable.scaledSize);
+               }
+               
+               // Step per millisecond
+               num step = (max(drawable.targetX, drawable.scaledSize) - min(drawable.targetX, drawable.scaledSize)) / duration;
+               
+               // Move our current location relative to the duration of the animation and time elapsed
+               if (isLeft) drawable.x -= step * deltaT;
+               else drawable.x += step * deltaT;
+               
+               // Check if weve gone past the target, if so, jump to the target and complete the animation
+               if ((isLeft && drawable.x < drawable.targetX) || (!isLeft && drawable.x > drawable.targetX)) {
+                  drawable.x = drawable.targetY;
+               }
+               else isComplete = false;
+             }
+              
+              if (directions.containsKey("up") || directions.containsKey("down")) {
+                bool isUp = directions["up"] == true;
+                
+                // Reset our target location so we can slide in or out
+                if (item.isFirstFrame) {
+                    if (isIn) {
+                      drawable.targetY = 0;
+                      drawable.y = (isUp  == true ? drawable.scaledSize : 0 - drawable.scaledSize);
+                    }
+                    else drawable.y = (isUp  == true ? 0 - drawable.scaledSize : drawable.scaledSize);
+                }
+                
+                // Step per millisecond
+                num step = (max(drawable.targetY, drawable.scaledSize) - min(drawable.targetY, drawable.scaledSize)) / duration;
+                // Move our current location relative to the duration of the animation and time elapsed
+                if (isUp) drawable.y -= step * deltaT;
+                else drawable.y += step * deltaT;
+                
+                // Check if weve gone past the target, if so, jump to the target and complete the animation
+                if ((isUp && drawable.y < drawable.targetY) || (!isUp && drawable.y > drawable.targetY)) {
+                   drawable.y = drawable.targetY;
+                }
+                else isComplete = false;
+              }
+              
+              return isComplete;
+            }
+     registerTransition(const Symbol("slide"), slide);
+     
+     // Slide ins
+     registerTransition(const Symbol ("slideInUp"), (FaviconDrawable drawable, num deltaT, List parameters, FaviconTransitionQueueItem item) {
+       return slide (drawable, deltaT, [{ "up": true }, parameters.length > 0 ? parameters[0] : 1000], item);
+     });
+     registerTransition(const Symbol ("slideInDown"), (FaviconDrawable drawable, num deltaT, List parameters, FaviconTransitionQueueItem item) {
+       return slide (drawable, deltaT, [{ "down": true }, parameters.length > 0 ? parameters[0] : 1000], item);
+     });
+     registerTransition(const Symbol ("slideInLeft"), (FaviconDrawable drawable, num deltaT, List parameters, FaviconTransitionQueueItem item) {
+       return slide (drawable, deltaT, [{ "left": true }, parameters.length > 0 ? parameters[0] : 1000], item);
+     });
+     registerTransition(const Symbol ("slideInRight"), (FaviconDrawable drawable, num deltaT, List parameters, FaviconTransitionQueueItem item) {
+       return slide (drawable, deltaT, [{ "right": true }, parameters.length > 0 ? parameters[0] : 1000], item);
+     });
+     
+     // Slide outs
+
+     registerTransition(const Symbol ("slideOutUp"), (FaviconDrawable drawable, num deltaT, List parameters, FaviconTransitionQueueItem item) {
+       return slide (drawable, deltaT, [{ "up": true }, parameters.length > 0 ? parameters[0] : 1000, false], item);
+     });
+     registerTransition(const Symbol ("slideOutDown"), (FaviconDrawable drawable, num deltaT, List parameters, FaviconTransitionQueueItem item) {
+       return slide (drawable, deltaT, [{ "down": true }, parameters.length > 0 ? parameters[0] : 1000, false], item);
+     });
+     registerTransition(const Symbol ("slideOutLeft"), (FaviconDrawable drawable, num deltaT, List parameters, FaviconTransitionQueueItem item) {
+       return slide (drawable, deltaT, [{ "left": true }, parameters.length > 0 ? parameters[0] : 1000, false], item);
+     });
+     registerTransition(const Symbol ("slideOutRight"), (FaviconDrawable drawable, num deltaT, List parameters, FaviconTransitionQueueItem item) {
+       return slide (drawable, deltaT, [{ "right": true }, parameters.length > 0 ? parameters[0] : 1000, false], item);
+     });
+     
+     
+     // Fades
+     
+     bool fade (FaviconDrawable drawable, num deltaT, List parameters, FaviconTransitionQueueItem item) {
+       int duration = (parameters.length >= 1 && parameters[0] is int ? parameters[0] : 1000);
+       num targetOpacity = (parameters.length >= 3 && parameters[1] is num ? parameters[1] : 1.0);
+       bool isComplete = false;
+       if (item.isFirstFrame) {
+         item.state["towards"] = drawable.opacity < targetOpacity; 
+         item.state["origOpacity"] = drawable.opacity; 
+         drawable.targetOpacity = targetOpacity;
+       }
+       num step = (drawable.targetOpacity - item.state["origOpacity"]) / duration;
+
+       drawable.opacity += (step * deltaT);
+       if ((item.state["towards"] && drawable.opacity > targetOpacity) || (!item.state["towards"] && drawable.opacity < targetOpacity)){
+         drawable.opacity = targetOpacity;
+         isComplete = true;
+       }
+       return isComplete;
+     }
+
+     registerTransition(const Symbol ("fade"), fade);
+     registerTransition(const Symbol ("fadeIn"), (FaviconDrawable drawable, num deltaT, List parameters, FaviconTransitionQueueItem item) {
+        return fade (drawable, deltaT, [parameters.length > 0 ? parameters[0] : 1000, 1.0], item);
+     });
+     registerTransition(const Symbol ("fadeOut"), (FaviconDrawable drawable, num deltaT, List parameters, FaviconTransitionQueueItem item) {
+        return fade (drawable, deltaT, [parameters.length > 0 ? parameters[0] : 1000, 0.0], item);
+     });
    }
  }
  noSuchMethod (Invocation invoke) {
    if (invoke.isMethod){
      if (FaviconDrawable._transitions.containsKey(invoke.memberName)) {
-       var fIQI = new FaviconTransitionQueueItem(invoke.memberName, parameters);
+       var fIQI = new FaviconTransitionQueueItem(invoke.memberName, invoke.positionalArguments);
        _currentQueue.add(fIQI);
       return fIQI.c.future;
      }
@@ -144,6 +359,7 @@ abstract class FaviconDrawable {
 }
 
 class FaviconTransitionQueue {
+  Completer c = new Completer();
   List<FaviconTransitionQueueItem> transitions = new List<FaviconTransitionQueueItem>();
   FaviconTransitionQueue();
   void add (FaviconTransitionQueueItem item) { 
@@ -153,6 +369,8 @@ class FaviconTransitionQueue {
 class FaviconTransitionQueueItem { // Probably too descriptive!
   Symbol animationName;
   Completer c = new Completer();
+  bool isFirstFrame = true;
+  Map state = new Map();
   List parameters;
   FaviconTransitionQueueItem (this.animationName, this.parameters);
 }
@@ -166,36 +384,137 @@ class FaviconPosition {
   static const FaviconPosition BOTTOM_RIGHT = const FaviconPosition(3);
 }
 
+class RGBA {
+  int r = 0;
+  int g = 0;
+  int b = 0;
+  num a = 0;
+  num alphaMod = 0;
+  
+  RGBA (this.r, this.g, this.b,[ this.a = 1.0 ]);
+  String toString () {
+    return "rgba($r, $g, $b, ${a - alphaMod})";
+  }
+  
+}
+
 class FaviconBadge extends FaviconDrawable {
-  String backgroundColour;
-  String fontFamily;
-  String fontStyle;
+  RGBA backgroundColor;
+  RGBA fontColor;
+  String font;
   String type;
   FaviconPosition position;
   List<int> badgeUpdateQueue = new List<int>();
   int currBadge = 0;
   
+  FaviconBadge ({ this.backgroundColor, this.fontColor, this.font: "30px Arial bold", this.type: "round", this.position: FaviconPosition.TOP_RIGHT }) {
+   if (this.backgroundColor == null) backgroundColor = new RGBA (255, 0, 0);
+   if (this.fontColor == null) fontColor = new RGBA (255,255,255);
+  }
+  
   void onAnimationQueueEnd () {
     if (badgeUpdateQueue.length > 0) {
+      print(badgeUpdateQueue[0]);
       currBadge = badgeUpdateQueue[0];
       badgeUpdateQueue.removeAt(0);
+      this.resume();
     }
+  }
+  String convertBadgeToText (int badgeNum) {
+
+    if (badgeNum > 999999) {
+       return "${(badgeNum / 1000000).toStringAsFixed(1)}M";
+    }
+    if (badgeNum > 999) {
+      return "${(badgeNum / 1000).toStringAsFixed(1)}K";
+    }
+    return badgeNum.toString();
   }
   
   void draw (CanvasRenderingContext2D ctx) {
+    
+    String badgeText = convertBadgeToText(currBadge);
+    
+   // fontColor.alphaMod = opacity;
+    ctx.font = "$font";   
+    double fW = ctx.measureText(badgeText).width;
+    double w = fW + 4;
+    // Estimated font height 
+    double h = fW / badgeText.length + 4;
+    
+    this.size = w;
+    backgroundColor.alphaMod = opacity;  
+    ctx.fillStyle = backgroundColor.toString();   
+    switch (position) {
+      case FaviconPosition.TOP_LEFT:
+        ctx.translate(2, 2);
+        break;
+      case FaviconPosition.TOP_RIGHT:
+        ctx.translate(parent.size - w - 2, 2);
+        break;
+      case FaviconPosition.BOTTOM_LEFT:
+        ctx.translate(2, parent.size - h - 2);
+        break;
+
+      case FaviconPosition.BOTTOM_RIGHT:
+        ctx.translate(parent.size - w - 2, parent.size - h - 2);
+        break;
+    }
+       
+    // TODO: Fix this properly
+    double tw = w;
+    double th = h;
+    if (type == "round") {
+      ctx.beginPath();
       
+      double w = x + tw;
+      double h = y + th;
+      ctx.lineTo(w - h / 2, y);
+      ctx.quadraticCurveTo(w, y, w, h / 2);
+      ctx.lineTo(w, h - h / 2);
+      ctx.quadraticCurveTo(w, h, w - h / 2, h);
+      ctx.lineTo(h / 2, h);
+      ctx.quadraticCurveTo(x, h, x, h - h / 2);
+      ctx.lineTo(x, h / 2);
+      ctx.quadraticCurveTo(x, y, h / 2, y);
+      ctx.fill();
+      ctx.closePath();
+    }
+    else {
+      ctx.fillRect(x, y, w, h);
+    }
+
+     fontColor.alphaMod = opacity;
+    ctx.fillStyle = fontColor.toString();
+    ctx.fillText(convertBadgeToText(currBadge), x + 2, y + (h - 1));  
+    
   }
   
-  void update (int deltaT) {
+  void update (num deltaT) {
      
   }
   
   void clearBadgeQueue() {
     badgeUpdateQueue = new List<int>();
   }
-  
-  void badge (int updateNumber) {
+  Future<FaviconDrawable> incrementBadge () {
+    _lastBadgeNum++;
+    return this.badge(_lastBadgeNum);
+  }
+  int _lastBadgeNum = 0;
+  Future<FaviconDrawable> badge (int updateNumber) {
+    //currBadge = updateNumber;
+    _lastBadgeNum = updateNumber;
     badgeUpdateQueue.add(updateNumber);
-    this.play();
+    return this.play();
+  }
+}
+
+class FaviconBackgroundSource extends FaviconDrawable {
+  RGBA backgroundColor;
+  FaviconBackgroundSource (this.backgroundColor);
+  void draw (CanvasRenderingContext2D ctx) {
+    ctx.fillStyle = backgroundColor.toString();
+    ctx.fillRect(x, y, this.size, this.size);
   }
 }
